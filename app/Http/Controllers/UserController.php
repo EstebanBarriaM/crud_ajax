0250ed0e31bf9e6868552ed327201d71628c7fde
Esteban Barria <estebanl.barriam@gmail.com>
2024-05-21 04:13:07 -0400

    Vista USUARIOS INDEX, CONTROLADOR INDEX, DENTRO DE VISTA USUARIOS INDEX - AJAX PARA LISTADO DE TODOS LOS USUARIOS

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use DataTables;
 
 class UserController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        if($request->ajax()) {
 
+            $data = User::latest()->get();
+
+            return Datatables::of($data)
+                        ->addIndexColumn()
+                        ->addColumn('action', function($row) {
+
+                            $btn = '<a href="javascript:void(0)" data-toggle="tooltip" data-id="'.$row->id.'" data-original-title="Edit" class="edit btn btn-primary btn-sm editProduct">Editar</a>';
+                            $btn = $btn.'<a href="javascript:void(0)" data-toggle="tooltip" data-id="'.$row->id.'" data-original-title="Delete" class="btn btn-danger btn-sm deleteProduct">Eliminar</a>';
+
+                            return $btn;
+                        })
+                        ->rawColumns(['action'])
+                        ->make(true);
+
+        };
+
+        return view('users.index');
     }
 
     public function create()
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Esteban',
+            'last_name' => 'Barria',
+            'username' => 'dANAv',
+            'email' => 'estebanl.barriam@gmail.com',
+            'password' => bcrypt('password')
+        ]);
+    }
+}
